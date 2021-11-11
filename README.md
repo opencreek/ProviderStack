d870b519eb1744d3974d3803d90f4db1b5af9819
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-11-11 21:31:40 +0100

    :art: :rotating_light:

---
+++ README.md
@@ -0,0 +1,39 @@
+## Provider Stack
+
+Are you tired of the mountain of react providers, just lying in your app.tsx?
+This simple library solves that for you!
+
+## Usage
+
+```tsx
+// app.tsx
+import { providers, ProviderStack } from "@opencreek/provider-stack"
+
+
+
+// Somewhere in your app component
+
+const theme = //...
+
+const providerStack = useMemo(() =>
+    providers()
+        .add(ThemeProvider, {theme: theme})
+        .add(SomeOtherProvider),
+    [theme] // make sure to add everything that needs to trigger an update
+)
+
+
+return (
+    <ProviderStack providers={providerStack}>
+        // Rest of your app
+    </ProviderStack>
+)
+```
+
+### Some more Detail
+
+```tsx
+providers()
+    .add(ThemeProvider, { theme: theme }) // The second parameter are the props of the provider (excluding `children`)
+    .add(SomeOtherProvider) // If no props are needed, you can just leave them out!
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,67 @@
 import type { ComponentType, ReactElement, ReactNode } from "react"
 
-type Provider = {
-  add<T>(provider: ComponentType<T>, props?: Omit<T, "children">): Provider
-  // (props: { children: ReactNode }): ReactElement
-  providers: Array<[ComponentType<unknown>, unknown]>
+/**
+ * Represents a Stack of provider
+ */
+type ProviderStack = {
+    /**
+     * Adds a new provider
+     * @param provider The Provider to add
+     * @param props Optional pros for the provider
+     */
+    add<T>(
+        provider: ComponentType<T>,
+        props?: Omit<T, "children">
+    ): ProviderStack
+    /**
+     * The whole stack in order.
+     * Tuple of provider and props
+     */
+    providers: Array<[ComponentType<unknown>, unknown]>
 }
 
+/**
+ * Renders all configured providers in order
+ * @param children react children
+ * @param providers The providerStack
+ * @constructor
+ */
 export function ProviderStack({
-  children,
-  providers: currentProviders,
+    children,
+    providers,
 }: {
-  children: ReactNode
-  providers: Array<[ComponentType<unknown>, unknown]>
+    children: ReactNode
+    providers: ProviderStack
 }): ReactElement {
-  const provider = currentProviders?.reduceRight((cur, [Provider, props]) => {
-    return <Provider {...props}>{cur}</Provider>
-  }, <>{children}</>)
+    const provider = providers.providers?.reduceRight(
+        (cur, [Provider, props]) => {
+            return <Provider {...props}>{cur}</Provider>
+        },
+        <>{children}</>
+    )
 
-  return <>{provider}</>
+    return <>{provider}</>
 }
 
-export function providers(
-  currentProviders?: Array<[ComponentType<unknown>, unknown]>
-): Provider {
-  return {
-    add: function <T>(provider: ComponentType<T>, props?: Omit<T, "children">) {
-      // @ts-ignore
-      return providers([...(currentProviders ?? []), [provider, props]])
-    },
-    providers: currentProviders ?? [],
-  }
+/**
+ * Creates a ProviderStack.
+ * On the provider Stack you can add new providers with `.add(Provider)`
+ */
+export function providers(): ProviderStack {
+    return stack([])
+}
+
+function stack(
+    currentProviders: Array<[ComponentType<unknown>, unknown]>
+): ProviderStack {
+    return {
+        add: function <T>(
+            provider: ComponentType<T>,
+            props?: Omit<T, "children">
+        ) {
+            // @ts-ignore
+            return stack([...(currentProviders ?? []), [provider, props]])
+        },
+        providers: currentProviders ?? [],
+    }
 }
